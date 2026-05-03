0752ce22336735f7d2136503d385add0d63a1f3a
Aryan-Dot-Dev <aryan.main@proton.me>
2026-05-03 11:31:35 +0000

    Update AWS endpoints in Terraform configuration to use localstack

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://host.docker.internal:4566"
-    s3  = "http://host.docker.internal:4566"
+    ec2 = "http://localstack:4566"
+    s3  = "http://localstack:4566"
   }
 }
 
